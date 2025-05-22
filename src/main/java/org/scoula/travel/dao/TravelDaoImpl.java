b81e40d5aa83f44b069eecbbd779fbbe39960ecd
Eunsoo Kim <7.8204620u@gmail.com>
2025-05-22 11:15:01 +0900

    ADD TravelDaoImpl

--- src/main/java/org/scoula/travel/dao/TravelDaoImpl.java
+++ src/main/java/org/scoula/travel/dao/TravelDaoImpl.java
@@ -6,7 +6,11 @@ import org.scoula.travel.domain.TravelVO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 
 public class TravelDaoImpl implements TravelDao{
@@ -39,5 +43,109 @@ public class TravelDaoImpl implements TravelDao{
             throw new RuntimeException(e);
         }
 
+    }
+
+    @Override
+    public int getTotalCount() {
+        String sql = "select count(*) from tbl_travel";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<String> getDistricts() {
+        List<String> districts = new ArrayList<>();
+        String sql = "SELECT DISTINCT(district) FROM tbl_travel ORDER BY district";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+            while (rs.next()) {
+                districts.add(rs.getString("district"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return districts;
+    }
+
+    private TravelVO map(ResultSet rs) throws SQLException
+    {
+        return TravelVO.builder()
+                .no(rs.getLong("no"))
+                .district(rs.getString("district"))
+                .title(rs.getString("title"))
+                .description(rs.getString("description"))
+                .address(rs.getString("address"))
+                .phone(rs.getString("phone"))
+                .build();
+    }
+
+
+    @Override
+    public List<TravelVO> getTravels() {
+        List<TravelVO> travels = new ArrayList<>();
+        String sql = "select * from tbl_travel order by district, title";
+        try (PreparedStatement pstmt = conn.prepareStatement
+                (sql);
+             ResultSet rs = pstmt.executeQuery()) {
+            while (rs.next()) {
+                TravelVO travel = map(rs);
+                travels.add(travel);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return travels;
+
+    }
+
+    @Override
+    public List<TravelVO> getTravels(int page) {
+        List<TravelVO> travels = new ArrayList<>();
+        String sql = "select * from tbl_travel order by district, title limit ?,?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            int count = 10;
+            int start = (page - 1) * count;
+            pstmt.setInt(1, start);
+            pstmt.setInt(2, count);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    TravelVO travel = map(rs);
+                    travels.add(travel);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return travels;
+    }
+
+    @Override
+    public List<TravelVO> getTravels(String district) {
+        List<TravelVO> travels = new ArrayList<>();
+        String sql = "select * from tbl_travel where district = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement
+                (sql)) {
+            pstmt.setString(1, district);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    TravelVO travel = map(rs);
+                    travels.add(travel);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return travels;
+
+    }
+
+    @Override
+    public Optional<TravelVO> getTravel(Long no) {
+        return Optional.empty();
     }
 }
